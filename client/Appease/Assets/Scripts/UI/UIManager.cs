74597d55257b7911f6e607ce9285fd2d5a09ca8a
Ozan Ürkmez <arkantos250@gmail.com>
2020-12-16 15:56:39 +0300

    organize some namespaces and import Tom Weiland's packet code

---
+++ client/Appease/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+
+    using Networking;
+
+    public class UIManager : MonoBehaviour
+    {
+
+        public static UIManager Singleton { get; private set; }
+
+        [SerializeField]
+        private GameObject startMenu;
+        [SerializeField]
+        private InputField usernameField;
+
+        #region Singleton Architecture
+
+        private void Awake()
+        {
+            if (Singleton != null)
+            {
+                Destroy(this);
+                return;
+            }
+            Singleton = this;
+        }
+
+        private void OnDestroy()
+        {
+            if (Singleton == this)
+                Singleton = null;
+        }
+
+
+        #endregion
+
+        public void ConnectToServer()
+        {
+            startMenu.SetActive(false);
+            usernameField.interactable = false;
+            Client.Singleton.ConnectToServer();
+        }
+
+
+    }
+
+}
+
