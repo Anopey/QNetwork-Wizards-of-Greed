006a21125d198533bf480abda973aa497833bd96
Ozan Ürkmez <arkantos250@gmail.com>
2020-12-24 09:04:49 +0300

    packet manager base struct types and constructor

--- server/packet/packetManager.go
+++ server/packet/packetManager.go
@@ -2,8 +2,21 @@ package packet
 
 import "reflect"
 
+type PacketHandler func(pd *PacketData, p *Packet)
+
 type PacketData struct {
 	id         int
 	primitives []reflect.Type
+	handler    *PacketHandler
 }
 
+type PacketManager struct {
+	packets map[uint16]*PacketData
+}
+
+func NewPacketManager() *PacketManager {
+	pm := PacketManager{
+		packets: make(map[uint16]*PacketData),
+	}
+	return &pm
+}
