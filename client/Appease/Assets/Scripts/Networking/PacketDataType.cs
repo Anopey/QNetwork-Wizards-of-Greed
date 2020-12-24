d81d96c5b5c13348a2826293a8e4ba3ca09383f5
Ozan Ürkmez <arkantos250@gmail.com>
2020-12-24 13:39:42 +0300

    packet minimum length calculation on the client side

--- client/Appease/Assets/Scripts/Networking/PacketDataType.cs
+++ client/Appease/Assets/Scripts/Networking/PacketDataType.cs
@@ -18,6 +18,81 @@ namespace Game.Networking
 
         public TypeCode[] Primitives;
 
+        private ushort calculatedMinimumTotalByteLength = ushort.MaxValue;
+
+        /// <summary>
+        /// This is called minimum since a string can be indefinitely long.
+        /// </summary>
+        /// <returns></returns>
+        public ushort GetMinimumByteLength()
+        {
+            if(calculatedMinimumTotalByteLength == ushort.MaxValue)
+            {
+                calculatedMinimumTotalByteLength = 0;
+                foreach(TypeCode t in Primitives)
+                {
+                    switch (t)
+                    {
+                        case TypeCode.Object:
+                            calculatedMinimumTotalByteLength += 4;
+                            break;
+                        case TypeCode.DBNull:
+                            calculatedMinimumTotalByteLength += 4;
+                            break;
+                        case TypeCode.Boolean:
+                            calculatedMinimumTotalByteLength += 1;
+                            break;
+                        case TypeCode.Char:
+                            calculatedMinimumTotalByteLength += 2;
+                            break;
+                        case TypeCode.SByte:
+                            calculatedMinimumTotalByteLength += 1;
+                            break;
+                        case TypeCode.Byte:
+                            calculatedMinimumTotalByteLength += 1;
+                            break;
+                        case TypeCode.Int16:
+                            calculatedMinimumTotalByteLength += 2;
+                            break;
+                        case TypeCode.UInt16:
+                            calculatedMinimumTotalByteLength += 2;
+                            break;
+                        case TypeCode.Int32:
+                            calculatedMinimumTotalByteLength += 4;
+                            break;
+                        case TypeCode.UInt32:
+                            calculatedMinimumTotalByteLength += 4;
+                            break;
+                        case TypeCode.Int64:
+                            calculatedMinimumTotalByteLength += 8;
+                            break;
+                        case TypeCode.UInt64:
+                            calculatedMinimumTotalByteLength += 8;
+                            break;
+                        case TypeCode.Single:
+                            calculatedMinimumTotalByteLength += 4;
+                            break;
+                        case TypeCode.Double:
+                            calculatedMinimumTotalByteLength += 8;
+                            break;
+                        case TypeCode.Decimal:
+                            calculatedMinimumTotalByteLength += 16;
+                            break;
+                        case TypeCode.DateTime:
+                            calculatedMinimumTotalByteLength += 8;
+                            break;
+                        case TypeCode.String:
+                            calculatedMinimumTotalByteLength += 4;
+                            break;
+                        default:
+                            Debug.LogError("Unexpected type code during packet data type length calculation");
+                            break;
+                    }
+                }
+                
+            }
+            return calculatedMinimumTotalByteLength;
+        }
     }
 
 }
--- client/Appease/Assets/Scripts/Networking/PacketManager.cs
+++ client/Appease/Assets/Scripts/Networking/PacketManager.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Game.Networking
 {
     public class PacketManager
     {
 
+        private Dictionary<int, PacketDataType> idToPacketDataType;
+
         public PacketManager(PacketManagerArgs packetManagerArgs)
         {
+            idToPacketDataType = new Dictionary<int, PacketDataType>();
 
+            foreach(var data in packetManagerArgs.handledTypes)
+            {
+                if (idToPacketDataType.ContainsKey(data.ID))
+                {
+                    Debug.LogError("Two packet types of same ID were tried to be handled in the same scene!");
+                }
+                idToPacketDataType.Add(data.ID, data);
+            }
         }
 
     }
