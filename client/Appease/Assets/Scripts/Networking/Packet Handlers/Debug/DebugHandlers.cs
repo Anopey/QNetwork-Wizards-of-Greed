1b867b666f3a4061444022faeb0ae88c329a8cf0
Ozan Ürkmez <arkantos250@gmail.com>
2020-12-31 11:41:58 +0300

    create some extra read utilities for convenience.

--- client/Appease/Assets/Scripts/Networking/Packet Handlers/Debug/DebugHandlers.cs
+++ client/Appease/Assets/Scripts/Networking/Packet Handlers/Debug/DebugHandlers.cs
@@ -8,14 +8,21 @@ namespace Game.Debugging
     using System;
 
     [CreateAssetMenu(fileName = "DebugFirstString", menuName = "Networking/PacketHandlers/DebugFirstString", order = 1)]
-    public class DebugHandlers : PacketHandler, IStaticPacketHandler
+    public class DebugHandlers : PacketHandler, IDynamicPacketHandler
     {
-        private TypeCode[] expected = new TypeCode[] { TypeCode.String};
+        private TypeCode[] expected;
 
-        public TypeCode[] ExpectedPrimitives { get { return expected; } }
+
+        public bool VerifyInitializePacket(TypeCode[] inputTypes)
+        {
+            expected = inputTypes;
+            return true;
+        }
 
         protected override void ProcessPacket(Packet packet)
         {
+            string msg = "Server sent: ";
+            
             Debug.Log("Server sent: " + packet.ReadString());
         }
     }
