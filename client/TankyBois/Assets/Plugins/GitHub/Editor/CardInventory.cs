ae04905486738a3e81ff28de147f75fbcf8c5d8c
Carol Xu <carolx715@gmail.com>
2021-01-04 15:42:44 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/Anopey/Appease into main""
    This reverts commit 5eb925432af071ac9a4c6135cb2ef0e7c591c698.

---
+++ client/TankyBois/Assets/Plugins/GitHub/Editor/CardInventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInventory
+{
+    
+    struct Card
+    {
+        int t1Spice;
+        int t2Spice;
+        int t3Spice;
+        int t4Spice;
+        bool usable;
+    }
+
+    List<Card> cards = new List<Card>();
+
+    public CardInventory()
+    {
+
+
+    }
+}
