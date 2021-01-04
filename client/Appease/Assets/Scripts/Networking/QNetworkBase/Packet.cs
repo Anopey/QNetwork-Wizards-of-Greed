33eadd53ffbbfb25ce0b880940a374d47afe55d2
Ozan Ürkmez <arkantos250@gmail.com>
2021-01-04 23:08:12 +0300

    namespace renaming and basis of addons

--- client/Appease/Assets/Scripts/Networking/QNetworkBase/Packet.cs
+++ client/Appease/Assets/Scripts/Networking/QNetworkBase/Packet.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Game.Networking
+namespace Game.QNetwork
 {
 
     //TODO consider getting rid of movereadpos optional bool
---
+++ client/Appease/Assets/Scripts/Networking/QStandardAttributeAddon/QStandardAttributeAddon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.QNetwork.QStandardAttributeInfo
+{
+
+    using QNetwork;
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "DebugFirstString", menuName = "Networking/DefaultPacketHandlers/DebugFirstString", order = 1)]
+    public class QStandardAttributeAddon : QAddon
+    {
+        protected override void OnNetworkManagerInitialized()
+        {
+            Debug.Log("QNetworking standard attribute addon is now being initialized...");
+        }
+    }
+}
