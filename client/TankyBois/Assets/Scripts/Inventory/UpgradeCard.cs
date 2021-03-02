7567daa554d313a4059dcffef6b3912815494b98
WolfVD <57068085+WolfVD@users.noreply.github.com>
2021-03-02 17:06:59 -0500

    upgrade card logic

--- client/TankyBois/Assets/Scripts/Inventory/UpgradeCard.cs
+++ client/TankyBois/Assets/Scripts/Inventory/UpgradeCard.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeCard : Card
 {
@@ -10,6 +15,11 @@ public class UpgradeCard : Card
         get;
         private set;
     }
+
+    int lastUpgrade = -1;
+    int upgradesLeft = 0;
+
+    private Coroutine upgradeProcess;
 
     public UpgradeCard(int upgradeCount = 0, bool usable = true)
     {
@@ -21,10 +31,47 @@ public class UpgradeCard : Card
     {
         if (!usable) return false;
 
-        
+        upgradesLeft = upgradeCount;
+
+        ProcessUpgrade(spiceInventory);
 
         usable = false;
         return true;
+    }
+
+    IEnumerator ProcessUpgrade(SpiceInventory spiceInventory)
+    {
+        if (lastUpgrade == 1)
+        {
+            spiceInventory.t1SpiceCount -= 1;
+            spiceInventory.t2SpiceCount += 1;
+            upgradesLeft -= 1;
+            lastUpgrade = -1;
+        }
+        else if (lastUpgrade == 2)
+        {
+            spiceInventory.t2SpiceCount -= 1;
+            spiceInventory.t3SpiceCount += 1;
+            upgradesLeft -= 1;
+            lastUpgrade = -1;
+        }
+        else if (lastUpgrade == 3)
+        {
+            spiceInventory.t3SpiceCount -= 1;
+            spiceInventory.t4SpiceCount += 1;
+            upgradesLeft -= 1;
+            lastUpgrade = -1;
+        }
+
+        if (upgradeCount > 0)
+        {
+            yield return new WaitWhile(() => lastUpgrade == -1);
+        }
+    }
+    
+    public void UseUpgrade(int spiceTier)
+    {
+        lastUpgrade = spiceTier;
     }
 
     public override Card GenerateCard()
